eec73ea97fd0680c7609c8e4a44ff9e4c4d7ecad
Poisk <vpisotski@gmail.com>
2018-04-23 20:45:35 -0700

    halfway on getelementsbyClassName JSON completed until objects

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,38 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+var getElementsByClassName = function(className) {
+//var getElementsByClassName = function(className, node, arrayOfClasses) {
+
+  // node = node || document.body;
+  // if (arrayOfClasses === undefined) {
+  //   arrayOfClasses === [];
+  // }
+  
+  // _.each(node, function(element) {
+  //   if (element.childNodes) {
+  //     getElementsByClassName(element.childNodes);
+  //   } else if ( element.className === className ) {
+  //     arrayOfClasses.push(element);
+  //   }
+  // });
+
+  // return arrayOfClasses; 
+
+  var arrayOfClasses = [];
+  var doc = document.body;
+
+  var bodySearch = function(body) {
+    if (doc.classList && ._contains(doc.classList, className) {
+      arrayOfClasses.push(body);
+    })
+    if (doc.childNodes) {
+      for (var i = 0; i < doc.childNodes.length; i++) {
+        bodySearch(doc.childNodes[i]);
+      }
+    }
+  } 
+
+  return arrayOfClasses;
 };
